55d22f489e273d54a8c9e46c73568f4a26944170
Dorian Goepp <dorian.goepp@gmail.com>
2015-08-27 08:54:03 +0200

    correct indentation

--- src/hexapod_server.cpp
+++ src/hexapod_server.cpp
@@ -2,37 +2,38 @@
 #include "hexa_control/Transfert.h"
 
 #include "robotHexa.hpp"
+#include "controllerDuty.hpp"
 
 #include <boost/shared_ptr.hpp>
 
-boost::shared_ptr<RobotHexa> hexapod_p; 
+boost::shared_ptr<RobotHexa> hexapod_p;
 ros::Publisher statepub;
 
 bool transfert(hexa_control::Transfert::Request  &req,
-         hexa_control::Transfert::Response &res )
+               hexa_control::Transfert::Response &res )
 {
   if(req.duration<-1)
-    {
-      hexapod_p->relax();
-      return true;
-    }
+  {
+    hexapod_p->relax();
+    return true;
+  }
   else if(req.duration ==0)
-    {  
-      hexapod_p->reset();
-      return true;
-    }
+  {
+    hexapod_p->reset();
+    return true;
+  }
   else if(req.duration ==-1)
-    {
-      hexapod_p->position_zero();
-      return true;
-    }
+  {
+    hexapod_p->position_zero();
+    return true;
+  }
 
   std::vector<float> params;
   for(int i=0;i<36;i++)
-    {
-      params.push_back(req.params[i]);
-      ROS_INFO("%f",params[i]);
-    }
+  {
+    params.push_back(req.params[i]);
+    ROS_INFO("%f",params[i]);
+  }
 
   ControllerDuty ctrl(params,std::vector<int>());
 
@@ -41,18 +42,18 @@ bool transfert(hexa_control::Transfert::Request  &req,
   statepub.publish(msg);
 
   //hexapod_p->initial_pos();
-try
+  try
   {
-    
-    hexapod_p->transfer(ctrl,req.duration,0); 
-    
+
+    hexapod_p->transfer(ctrl,req.duration,0);
+
   }
-catch (dynamixel::Error e)
-   {
-     std::cerr << "error (dynamixel): " << e.msg() << std::endl;
-     std::cout<<"closing serials"<<std::endl;
-     hexapod_p->close_usb_controllers();
-   }                      
+  catch (dynamixel::Error e)
+  {
+    std::cerr << "error (dynamixel): " << e.msg() << std::endl;
+    std::cout<<"closing serials"<<std::endl;
+    hexapod_p->close_usb_controllers();
+  }
 
   msg.data = "stop";
   statepub.publish(msg);
@@ -65,17 +66,17 @@ catch (dynamixel::Error e)
 
 int main(int argc, char **argv)
 {
-  //boost::shared_ptr<dynamixel::Usb2Dynamixel controller> controller; 
-try
+  //boost::shared_ptr<dynamixel::Usb2Dynamixel controller> controller;
+  try
   {
-  hexapod_p = boost::shared_ptr<RobotHexa>(new RobotHexa);    
+    hexapod_p = boost::shared_ptr<RobotHexa>(new RobotHexa);
   }
-catch (dynamixel::Error e)
-   {
-     std::cerr << "error (dynamixel): " << e.msg() << std::endl;
-     std::cout<<"closing serials"<<std::endl;
-     hexapod_p->close_usb_controllers();
-   }                      
+  catch (dynamixel::Error e)
+  {
+    std::cerr << "error (dynamixel): " << e.msg() << std::endl;
+    std::cout<<"closing serials"<<std::endl;
+    hexapod_p->close_usb_controllers();
+  }
 
 
   ros::init(argc, argv, "hexapod_server");
@@ -89,4 +90,3 @@ catch (dynamixel::Error e)
   hexapod_p->relax();
   return 0;
 }
-
