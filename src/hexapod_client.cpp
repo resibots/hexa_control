5f7bf8c50769d7e5bea845e1de11b97532fb3572
Dorian Goepp <dorian.goepp@inria.fr>
2015-12-08 18:18:29 +0100

    Update the file parser of hexapod_client.
    So that it can read the files outputed by limbo after an experiment
    (files named transferts.dat).

--- src/hexapod_client.cpp
+++ src/hexapod_client.cpp
@@ -1,12 +1,13 @@
-#include <ros/ros.h>
+// Package-specific includes
 #include <hexa_control/Transfert.h>
+
+// ROS includes
+#include <ros/ros.h>
+
+// Standard includes
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
-
-
-
-
 
 int main(int argc, char **argv)
 {
@@ -22,28 +23,59 @@ int main(int argc, char **argv)
   hexa_control::Transfert srv;
 
   //  std::ifstream monFlux("/home/antoinecully/fuerte_workspace/sandbox/hexa_control/results.dat");  //Ouverture d'un fichier en lecture
-  std::ifstream monFlux(argv[1]);  //Ouverture d'un fichier en lecture
+  std::ifstream monFlux(argv[1]); // Open the transfer file for reading
 
   std::vector<std::vector<float> > controllers;
   if(monFlux)
   {
+    // Old broken way to parse the file, since the format evolved
+    // Yet, the example files should be removed beforhand.
+    // while(!monFlux.eof())
+    // {
+    //   std::vector<float> controller;
+    //   for(int i =0;i<43;i++)
+    //   {
+    //     if(monFlux.eof())
+    //       break;
+    //     float data;
+    //     monFlux>>data;
+    //     //	      ROS_INFO("%d",data);
+    //     if(i>=7)
+    //       controller.push_back(data);
+    //     //  std::cout<<data<<" ";
+    //   }
+    //   if(controller.size()==36)
+    //     controllers.push_back(controller);
+    //   //	  std::cout<<std::endl;
+    // }
+    monFlux.ignore(100, '\n');
+    bool even_line = true;
     while(!monFlux.eof())
     {
-      std::vector<float> controller;
-      for(int i =0;i<43;i++)
+      if(even_line)
       {
-        if(monFlux.eof())
-          break;
-        float data;
-        monFlux>>data;
-        //	      ROS_INFO("%d",data);
-        if(i>=7)
-          controller.push_back(data);
-        //  std::cout<<data<<" ";
+        monFlux.ignore(100, '\n');
+        even_line = !even_line;
+        std::cout << "ignored line" << std::endl;
       }
-      if(controller.size()==36)
-        controllers.push_back(controller);
-      //	  std::cout<<std::endl;
+      else
+      {
+        std::vector<float> controller;
+        for(int i=0; i<36; i++)
+        {
+          if(monFlux.eof())
+            break;
+          float controller_parameter;
+          monFlux >> controller_parameter;
+          controller.push_back(controller_parameter);
+        //   std::cout << controller_parameter << " ";
+        }
+        even_line = !even_line;
+        if (controller.size() == 36)
+            controllers.push_back(controller);
+        monFlux.ignore(1, '\n');
+        // std::cout << "\n";
+      }
     }
   }
   else
